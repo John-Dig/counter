5ac8934c93c220eb7ec2062e2a17f084b2039835
John Diggins <rohnfiggins@gmail.com>
2023-05-01 10:52:35 -0700

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Example from './example';
+import Counter from './counter';
+
+
+function App() {
+  return (
+    <>
+<Counter />  
+    </>
+  );
+}
+
+export default App;
+
+
